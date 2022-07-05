1e53c190012ab60afc6407eeed74ad778876a630
fbansept <bansept.franck@gmail.com>
2022-07-05 11:03:44 +0200

    Partie 1 (structure de l'application)

---
+++ src/Bouton/Bouton.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Bouton.scss'
+
+export default function Bouton() {
+  return (
+      <button>Clic moi</button>
+  )
+}
---
+++ src/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Bouton from '../Bouton/Bouton'
+import './Header.scss'
+
+export default function Header() {
+    return (
+        <header>
+            <div className="conteneur-gauche">
+                <img className="logo" src={process.env.PUBLIC_URL + "/logo.png"} />
+                <ul className="menu">
+                    <li>Dashboard</li>
+                    <li>Utilisateurs</li>
+                    <li>Produits</li>
+                    <li>Catégories</li>
+                </ul>
+            </div>
+            <div className="conteneur-droite">
+                <Bouton></Bouton>
+                <Bouton></Bouton>
+            </div>
+
+        </header>
+    )
+}
