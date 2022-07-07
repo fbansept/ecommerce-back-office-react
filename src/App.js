ca9586da8095ac2b49a9d1b8662d6be16dc75f91
fbansept <bansept.franck@gmail.com>
2022-07-07 11:28:48 +0200

    Fix router position

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './Header/Header';
 import SideMenu from './SideMenu/SideMenu';
 import { useState } from 'react';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Dashboard from './pages/Dashboard/Dashboard';
 import Utilisateurs from './pages/Utilisateurs/Utilisateurs';
 import Produits from './pages/Produits/Produits';
@@ -11,33 +11,10 @@ import Page404 from './pages/Page404/Page404';
 
 function App() {
 
-  // const listeSousMenu = {
-  //   dashboard : [
-  //     { titre: "Chiffre d'affaire", icone: "circle-dollar" },
-  //     { titre: "Stock", icone: "shelves" },
-  //     { titre: "Commandes", icone: "boxes-packing" }
-  //   ],
-  //   utilisateurs: [
-  //     { titre: "Gérer les utilisateurs", icone: "users" },
-  //     { titre: "Exporter la liste", icone: "file-export" },
-  //     { titre: "Importer la liste", icone: "file-import" }
-  //   ],
-  //   produits: [
-  //     { titre: "Gérer les produits", icone: "shelves" },
-  //     { titre: "Exporter la liste", icone: "file-export" },
-  //     { titre: "Importer la liste", icone: "file-import" }
-  //   ], 
-  //   categories: [
-  //     { titre: "Gérer les categories", icone: "folder-tree" },
-  //     { titre: "Exporter la liste", icone: "file-export" },
-  //     { titre: "Importer la liste", icone: "file-import" }
-  //   ]
-  // }
-
   const [utilisateurConnecte, setUtilisateurConnecte] = useState(null);
   
   return (
-    <BrowserRouter>
+    <>
       <Header utilisateurConnecte={utilisateurConnecte} 
               setUtilisateurConnecte={setUtilisateurConnecte}>
       </Header>
@@ -54,7 +31,7 @@ function App() {
             </Routes>
         </div>
       </div>
-    </BrowserRouter>
+    </>
   );
 }
 
